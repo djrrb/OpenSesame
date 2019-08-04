5c80548f8d4f17e8b4d986742a0ff2ddb721b925
David Ross <david@djr.com>
2019-08-04 14:18:17 -0400

    add comments to sample script

--- README.md
+++ README.md
@@ -7,3 +7,25 @@ A quick RoboFont utility to give you a dialog that lets you choose if you want t
 * All open fonts
 
 ![preview.png](preview.png)
+
+## Using it
+
+```
+from utils.opensesame import OpenSesame
+# return a list of either fonts or paths
+for fontOrPath in OpenSesame.collect():
+    # get a font object
+    f = OpenSesame.getFont(fontOrPath, showUI=False)
+    # get a path object
+    print('Working on %s...' %OpenSesame.getPath(fontOrPath))
+    # work on glyphs
+    for g in f:
+        print('\tDo something to', g)
+        # ...
+        # put your code here!
+        # ...
+    # do you want to save the file only if it is unopened?
+    # OpenSesame.saveIfUnopened(fop)
+    f.close()
+print('done')
+```
--- sampleScript.py
+++ sampleScript.py
@@ -1,10 +1,17 @@
 from utils.opensesame import OpenSesame
 # return a list of either fonts or paths
-for fop in OpenSesame.collect():
-    f = OpenSesame.getFont(fop, showUI=False)
-    print('Working on %s...' %OpenSesame.getPath(fop))
+for fontOrPath in OpenSesame.collect():
+    # get a font object
+    f = OpenSesame.getFont(fontOrPath, showUI=False)
+    # get a path object
+    print('Working on %s...' %OpenSesame.getPath(fontOrPath))
+    # work on glyphs
     for g in f:
         print('\tDo something to', g)
-    #OpenSesame.saveIfUnopened(fop)
+        # ...
+        # put your code here!
+        # ...
+    # do you want to save the file only if it is unopened?
+    # OpenSesame.saveIfUnopened(fop)
     f.close()
 print('done')
